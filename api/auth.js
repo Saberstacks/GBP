4df8eed29b06b42fe8f93533420c2b280112d875
Saberstacks <saber7email@gmail.com>
2024-11-02 15:57:38 -0400

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,40 +1,42 @@
 // api/auth.js
 const { google } = require('googleapis');
-const querystring = require('querystring');
 const cookie = require('cookie');
 
 module.exports = async (req, res) => {
   if (req.method === 'GET') {
-    const { code, state } = req.query;
+    const { code } = req.query;
 
     if (!code) {
+      console.error('Authorization code not found in query parameters.');
       res.status(400).send('Missing authorization code');
       return;
     }
 
     try {
       const oauth2Client = new google.auth.OAuth2(
-        '1039601440969-6ca4vofk0c57p7hot5o759r5fcgvkfqe.apps.googleusercontent.com',
-        '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',
-        'https://gbp-index.vercel.app/login.html'
+        process.env.GOOGLE_CLIENT_ID,
+        process.env.GOOGLE_CLIENT_SECRET,
+        'https://gbp-index.vercel.app/api/auth'
       );
 
       const { tokens } = await oauth2Client.getToken(code);
       oauth2Client.setCredentials(tokens);
 
-      // Set tokens in a secure HTTP-only cookie
+      // Set tokens in secure HTTP-only cookies
       res.setHeader('Set-Cookie', [
         cookie.serialize('access_token', tokens.access_token, {
           httpOnly: true,
           secure: true,
           path: '/',
           sameSite: 'lax',
+          domain: 'gbp-index.vercel.app',
         }),
         cookie.serialize('refresh_token', tokens.refresh_token, {
           httpOnly: true,
           secure: true,
           path: '/',
           sameSite: 'lax',
+          domain: 'gbp-index.vercel.app',
         }),
       ]);
 
