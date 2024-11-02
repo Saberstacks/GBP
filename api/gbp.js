d32e388a4104adc49281a964aaed3cbaa5d444bb
Saberstacks <saber7email@gmail.com>
2024-11-02 15:58:06 -0400

    Update gbp.js

--- api/gbp.js
+++ api/gbp.js
@@ -17,25 +17,30 @@ module.exports = async (req, res) => {
       const oauth2Client = new google.auth.OAuth2();
       oauth2Client.setCredentials({ access_token: accessToken });
 
-      const mybusiness = google.mybusinessbusinessinformation({
+      const mybusinessaccountmanagement = google.mybusinessaccountmanagement({
         version: 'v1',
         auth: oauth2Client,
       });
 
       // Get the user's accounts
-      const accountsResponse = await mybusiness.accounts.list();
+      const accountsResponse = await mybusinessaccountmanagement.accounts.list();
       const accounts = accountsResponse.data.accounts;
 
       if (!accounts || accounts.length === 0) {
-        res.status(200).json({ message: 'No Google Business Profiles found.' });
+        res.status(200).json({ noGbp: true, message: 'No Google Business Profiles found.' });
         return;
       }
 
-      const accountId = accounts[0].name;
+      const accountName = accounts[0].name;
+
+      const mybusinessbusinessinformation = google.mybusinessbusinessinformation({
+        version: 'v1',
+        auth: oauth2Client,
+      });
 
       // Get locations for the account
-      const locationsResponse = await mybusiness.accounts.locations.list({
-        parent: accountId,
+      const locationsResponse = await mybusinessbusinessinformation.accounts.locations.list({
+        parent: accountName,
       });
 
       const locations = locationsResponse.data.locations;
